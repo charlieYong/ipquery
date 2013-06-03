b6061559e0ae82e8842ce0e02f6e88bc7e5ef4e5
charlieYong <charlie.szu@gmail.com>
2013-06-04 00:16:38 +0800

    add ustime() to t.c

--- t/t.c
+++ t/t.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/time.h>
 #include "src/ip.h"
+
+long long ustime() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (long long)tv.tv_sec*1000000 + tv.tv_usec;
+}
 
 int main(int argc, char** argv) {
     if (2 > argc) {
@@ -14,8 +21,11 @@ int main(int argc, char** argv) {
         return -1;
     }
     int i = 1;
+    long long start;
     for (; i<argc; i++) {
-        printf("%s -> %s\n", argv[i], query(argv[i]));
+        start = ustime();
+        printf("%s -> %s ", argv[i], query(argv[i]));
+        printf("time: %.6f seconds\n", (double)((ustime() - start) / 1000000));
     }
     release();
     return 0;
